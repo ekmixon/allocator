18e2afff5616567c35e624316eb82937c4cd2abb
Alexandre Dulaunoy <a@foo.be>
2016-03-08 21:28:54 +0100

    Mininal functions to create an ID, publish and so on

--- lib/allocator.py
+++ lib/allocator.py
@@ -4,6 +4,7 @@
 import os
 import configparser
 import datetime
+import time
 import redis
 
 commonpath = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +19,10 @@ def _getyear():
     return datetime.datetime.now().year
 
 
+def _getepoch():
+    return int(time.time())
+
+
 def _getprefix():
     return "{}-{}-".format(prefix, _getyear())
 
@@ -28,17 +33,62 @@ class identifier:
         self.db = redis.StrictRedis(host=config['redis']['host'],
                                     port=config['redis']['port'], db=3,
                                     charset="utf-8", decode_responses=True)
+        self.username = username
 
-    def new(self, title=None, description=None):
+    def new(self, title=None, description=None, state='Allocated'):
         val = self.db.incr(_getyear())
         identifier = "{}{}".format(_getprefix(), val)
+        hkey = "d:{}".format(identifier)
+        self.db.hset(hkey, 'requestor', self.username)
+        self.db.hset(hkey, 'title', title)
+        self.db.hset(hkey, 'description', description)
+        if state == "Allocated":
+            self.db.sadd('allocated', identifier)
+        self.db.hset(hkey, 'state', state)
+        self.db.hset(hkey, 'requested', _getepoch())
         return identifier
+
+    def addref(self, identifier=False, ref=False):
+        if not identifier or not ref:
+            return False
+        rkey = "r:{}".format(identifier)
+        hkey = "d:{}".format(identifier)
+        if not self.db.exists(hkey):
+            return False
+        return self.db.sadd(rkey, ref)
+
+    def publish(self, identifier=False):
+        if not identifier:
+            return False
+        hkey = "d:{}".format(identifier)
+        if not self.db.exists(hkey):
+            return False
+        self.db.hset(hkey, 'state', 'Published')
+        return self.db.sadd('published', identifier)
+
+    def expired(self, identifier=False):
+        if not identifier:
+            return False
+        hkey = "d:{}".format(identifier)
+        if not self.db.exists(hkey):
+            return False
+        if self.db.sismember('published', identifier):
+            return False
+        requested = int(self.db.hget(hkey, 'requested'))
+        now = int(_getepoch())
+        return int(now-requested)
+
+
 #
 # c:<year> - current counter (string)
-# d:<year>:id - {description} - value
+# d:<identifier> - {description} - value
 #               {title}       - value
 #               {state}       - allocated, published
-# r:<year>:id (set) of refs (URL)
+# d:<identifier> - {requestor}   - value
+# d:<identifier> - {requested} - value (epoch)
+# r:<identifier> (set) of refs (URL)
+# allocated (set of allocated identifiers)
+# published (set of published identifiers)
 # cpe:<year>:id (set) of cpe 2.3
 #
 # sub:<user> (set) of <year>:id
@@ -48,3 +98,6 @@ if __name__ == "__main__":
     print (_getprefix())
     i = identifier()
     print (i.new())
+    print (i.addref(identifier='SHVI-2016-2', ref='http://www.foo.be/'))
+    print (i.publish(identifier='SHVI-2016-2'))
+    print (i.expired(identifier='SHVI-2016-4'))
